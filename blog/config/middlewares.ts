69b2b74e5e94fbcedbaee3a8791b5eb18837ca31
Aman Kumar <103573041+amankumar-in@users.noreply.github.com>
2025-10-11 02:24:57 +0530

    bug

--- blog/config/middlewares.ts
+++ blog/config/middlewares.ts
@@ -7,7 +7,7 @@ export default [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['http://localhost:3000', 'http://localhost:5173', 'https://coinsforcollege.org','http://192.168.0.195:5173','http://192.168.0.195:4000'
+      origin: ['http://localhost:3000', 'http://localhost:5173', 'https://coinsforcollege.org','http://192.168.0.195:5173','http://192.168.0.195:4000',
         'https://www.coinsforcollege.org',
         'https://cfc-v2.onrender.com',process.env.CLIENT_URL].filter(Boolean),
       headers: '*',
