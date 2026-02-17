8e9bd34eed19cc8bcf42a0a914ac378057073246
Aman Kumar <103573041+amankumar-in@users.noreply.github.com>
2026-02-17 10:38:52 +0530

    chore: Add new Collegenz domains to CORS allowed origins.

--- blog/config/middlewares.ts
+++ blog/config/middlewares.ts
@@ -9,7 +9,7 @@ export default [
       enabled: true,
       origin: ['http://localhost:3000', 'http://localhost:3000', 'https://coinsforcollege.org','http://192.168.0.195:3000','http://192.168.0.195:4000',
         'https://www.coinsforcollege.org','http://192.168.0.16:3000', 'http://192.168.0.16:4000',
-        'https://cfc-v2.onrender.com',process.env.CLIENT_URL].filter(Boolean),
+        'https://cfc-v2.onrender.com','https://collegenz-marketing.onrender.com','https://collegenz.com','https://www.collegenz.com',process.env.CLIENT_URL].filter(Boolean),
       headers: '*',
     },
   },
